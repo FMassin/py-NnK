334702802c476e93f0ab8e5990de89a222dcb7ac
Frédérick Massin <fmassin@sed.ethz.ch>
2017-05-19 17:59:51 +0200

    Anticipate a future better structure.

---
+++ NnK/catalog_addons.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+source - Addon module for obspy catalog.
+
+This module provides additionnal functionnalities for event catalogs.
+______________________________________________________________________
+
+.. note::
+
+Functions and classes are ordered from general to specific.
+
+"""
+import obspy
---
+++ NnK/inventory_addons.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+source - Addon module for obspy inventory.
+
+This module provides additionnal functionnalities for station inventory.
+______________________________________________________________________
+
+.. note::
+
+Functions and classes are ordered from general to specific.
+
+"""
+import obspy
